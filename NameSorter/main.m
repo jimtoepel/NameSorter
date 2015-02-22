b8d810cd9f276b6acd5c98e1432f5dd60a4c83f0
jimtoepel <jim.toepel@gmail.com>
2015-02-22 15:18:35 -0800

    Comparing lists. Using Sets + arrays
    Need to remove Capitalized names from compare list

--- NameSorter/main.m
+++ NameSorter/main.m
@@ -10,8 +10,50 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        NSLog(@"Starting...");
+        
+        // read in the file as a huge string (ignoring errors)
+        
+        NSString *nameString = [NSString stringWithContentsOfFile:@"/usr/share/dict/propernames"
+                                                         encoding:NSUTF8StringEncoding
+                                                            error:NULL];
+
+        NSString *wordString = [NSString stringWithContentsOfFile:@"/usr/share/dict/words"
+                                                         encoding:NSUTF8StringEncoding
+                                                            error:NULL];
+        
+        NSLog(@"Strings loaded.");
+        
+        
+        // Break that up into an array of strings
+        NSArray *names = [nameString componentsSeparatedByString:@"\n"];
+        NSUInteger namesCount = [names count];
+        NSLog(@"Name array completed");
+        
+        NSMutableSet *words = [NSMutableSet setWithArray:[wordString componentsSeparatedByString:@"\n"]];
+        
+            //remove the empty string to avoid an extra match
+            [words removeObject:@""];
+        
+        NSUInteger wordsCount = [words count];
+        NSLog(@"Word array completed");
+       
+        
+        // Enumerate through the names array one at a time and ask if the lowercased name is a member of the words pile. (not interested in upper case)
+        
+        unsigned int total = 0;
+        
+        for (NSString *n in names) {
+                if ([words containsObject:[n lowercaseString]])
+                    NSLog(@"%@", n);
+            total ++;
+        }
+        
+        
+        NSLog(@"There are %lu words.\n", wordsCount);
+        NSLog(@"There are %lu names.\n", namesCount);
+        NSLog(@"There were %d matched, total.\n", total);
     }
     return 0;
 }
